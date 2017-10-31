cfb30d51eb9cabe8063f5bf64a46ff9225a4a5c4
ChristianAEDev <ChristianAE.DEV@gmail.com>
2017-10-31 11:01:22 +0000

    Add styling for day where no data has been logged

--- App.js
+++ App.js
@@ -1,16 +1,17 @@
-import React from 'react';
-import { View } from 'react-native';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-import reducer from './reducers';
-import AddEntry from './components/AddEntry';
-import History from './components/History';
+import React from "react";
+import { View } from "react-native";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import reducer from "./reducers";
+import AddEntry from "./components/AddEntry";
+import History from "./components/History";
 
 export default class App extends React.Component {
   render() {
     return (
       <Provider store={createStore(reducer)}>
-        <View style={{flex:1}}>
+        <View style={{ flex: 1 }}>
+          <View style={{ height: 20 }} />
           <History />
         </View>
       </Provider>
